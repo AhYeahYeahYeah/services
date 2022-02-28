b4f1c490e3ee17c49240615f2a250460e38bd160
ambrosecm <3021572573@qq.com>
2022-02-28 18:12:37 +0800

    edit controller

--- src/main/java/team/workflow/services/controller/StorageController.java
+++ src/main/java/team/workflow/services/controller/StorageController.java
@@ -1,12 +1,21 @@
 package team.workflow.services.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import team.workflow.services.service.StorageService;
+
+import javax.annotation.Resource;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/v1/storage")
 public class StorageController {
-
+    @Resource
+    private StorageService storageService;
+    @PostMapping("/lock")
+    public void StorageLock(String pid,String oid){
+        storageService.StorageLock(pid, oid);
+    }
 }
