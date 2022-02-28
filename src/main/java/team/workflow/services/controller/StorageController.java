052b2ce1b4bb82fb7cf928143eb473e0c07576fa
chooper666 <1721455274@qq.com>
2022-02-28 20:52:58 +0800

    solution for returning code

--- src/main/java/team/workflow/services/controller/StorageController.java
+++ src/main/java/team/workflow/services/controller/StorageController.java
@@ -1,9 +1,9 @@
 package team.workflow.services.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 import team.workflow.services.service.StorageService;
 
 import javax.annotation.Resource;
@@ -16,7 +16,15 @@ public class StorageController {
     private StorageService storageService;
     // 库存锁定
     @PostMapping("/lock")
-    public void StorageLock(String pid,String oid){
-        storageService.StorageLock(pid, oid);
+    public Mono<ResponseEntity> StorageLock(@RequestBody String pid, String oid){
+//        storageService.StorageLock(pid, oid);
+        return Mono.just(new ResponseEntity(HttpStatus.NOT_ACCEPTABLE));
+
     }
+//    @PostMapping("/lock1")
+//    public Mono<Integer> StorageLock1(@RequestBody String pid, String oid){
+////        storageService.StorageLock(pid, oid);
+//        return Mono.just(1);
+//
+//    }
 }
