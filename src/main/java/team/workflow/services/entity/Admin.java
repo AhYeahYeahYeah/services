000b8126a4791669eea6ac4500273c761b7d6640
chooper666 <1721455274@qq.com>
2022-03-05 15:52:23 +0800

    add column

--- src/main/java/team/workflow/services/entity/Admin.java
+++ src/main/java/team/workflow/services/entity/Admin.java
@@ -5,66 +5,74 @@ import org.springframework.data.annotation.Id;
 
 @Data
 public class Admin {
-  @Id
-  private String aid;
-  private String account;
-  private String password;
-  private String aname;
-  private String nickName;
-  private String avatar;
+    @Id
+    private String aid;
+    private String account;
+    private String password;
+    private String aname;
+    private String nickName;
+    private String avatar;
+    private String permissions;
+
+    public String getAid() {
+        return aid;
+    }
+
+    public void setAid(String aid) {
+        this.aid = aid;
+    }
 
 
-  public String getAid() {
-    return aid;
-  }
+    public String getAccount() {
+        return account;
+    }
 
-  public void setAid(String aid) {
-    this.aid = aid;
-  }
-
-
-  public String getAccount() {
-    return account;
-  }
-
-  public void setAccount(String account) {
-    this.account = account;
-  }
+    public void setAccount(String account) {
+        this.account = account;
+    }
 
 
-  public String getPassword() {
-    return password;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-
-  public String getAname() {
-    return aname;
-  }
-
-  public void setAname(String aname) {
-    this.aname = aname;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 
-  public String getNickName() {
-    return nickName;
-  }
+    public String getAname() {
+        return aname;
+    }
 
-  public void setNickName(String nickName) {
-    this.nickName = nickName;
-  }
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
 
 
-  public String getAvatar() {
-    return avatar;
-  }
+    public String getNickName() {
+        return nickName;
+    }
 
-  public void setAvatar(String avatar) {
-    this.avatar = avatar;
-  }
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(String permissions) {
+        this.permissions = permissions;
+    }
 
 }
